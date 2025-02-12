4c2009adf974ef376e8b4cfc5795851cc8e9c73d
wasomi <wasomi@yandex.ru>
2025-02-12 15:08:32 +0500

    some updates

---
+++ .config/hypr/scripts/screenshot.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+grim -g "$(slurp)" -t ppm - | satty --filename - --fullscreen --output-filename ~/Pictures/Screenshots/satty-$(date '+%Y%m%d-%H:%M:%S').png
--- .config/hypr/scripts/theme.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-gradience-cli apply -n "pywal" --gtk both
-kvantummanager --set "pywal"
