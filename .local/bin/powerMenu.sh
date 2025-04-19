fea086d8fb008781a4d79a5170a3c26447d407f9
wasomi <wasomi@yandex.ru>
2025-04-20 00:43:48 +0500

    update

--- .local/bin/powerMenu.sh
+++ .local/bin/powerMenu.sh
@@ -10,7 +10,7 @@
 # credits: https://github.com/Zproger
 # ----------------------------------------------------------------
 
-choice=$(printf "Lock\nSuspend\nReboot\nShutdown" | rofi -dmenu  -p "Power Menu" -config ~/.dotfiles/.config/rofi/styles/powerMenu.rasi)
+choice=$(printf "Lock\nSuspend\nReboot\nShutdown" | rofi -dmenu -p "Power Menu" -config ~/.dotfiles/.config/rofi/styles/powerMenu.rasi)
 
 case "$choice" in
   Lock) hyprlock ;;
