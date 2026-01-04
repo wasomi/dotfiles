b40fea16903376d23b0ca829b42abe5ccf0ad75a
wasomi <wasomi@yandex.ru>
2026-01-04 18:19:24 +0500

    update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,5 +18,5 @@ alias la='eza -a --color=always --group-directories-first --icons'  # all files 
 alias ll='eza -l --color=always --group-directories-first --icons'  # long format
 alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
 alias l.="eza -a | grep -e '^\.'"                                   # show only dotfiles
-alias h="hyprland"                                                  # alias to start hyprland
+alias h="start-hyprland"                                            # alias to start hyprland
 alias cat="bat"                                                     # alias to start bat
