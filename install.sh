01f98b11b82b51adb234f8a95502dd2eb8d2f5d6
wasomi <wasomi@yandex.ru>
2026-03-22 19:40:57 +0500

    fix

--- install.sh
+++ install.sh
@@ -162,7 +162,7 @@ read install_gaming
 if [[ $install_gaming =~ ^[Yy]$ ]]; then
     echo -e "${BLUE}[*]${NC} Installing gaming packages..."
     sudo pacman -S --needed steam gamemode mangohud
-    sudo paru -S --needed --noconfirm mangojuice-bin protonplus
+    paru -S --needed --noconfirm mangojuice-bin protonplus
     
     echo -e "${BLUE}[*]${NC} Adding user to gamemode group..."
     sudo usermod -aG gamemode "$USER"
